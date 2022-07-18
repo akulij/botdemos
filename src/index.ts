8dc11749db6e17c526e0496cfe8bbb66605cb24b
akulij <48361256+akulij@users.noreply.github.com>
2022-07-18 11:36:53 +0500

    add error when BOTTOKEN is not defined

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import { Telegraf, Context } from "telegraf";
 
 require("dotenv").config();
 
-const bot = new Telegraf(process.env.BOTTOKEN as string);
+if (typeof process.env.BOTTOKEN !== "string") {
+  console.error("Pass BOTTOKEN as enviroment variable or in .env file!");
+  process.exit(1);
+}
+
+const bot = new Telegraf(process.env.BOTTOKEN);
 
 bot.start((ctx: Context) => ctx.reply("Welcome"));
 bot.launch();
