1bd531794ec4553bdcd06bbf9d551744cf9e4f78
akulij <48361256+akulij@users.noreply.github.com>
2022-07-22 20:38:46 +0500

    add translation function for keyboards

--- src/modules/translation/index.ts
+++ src/modules/translation/index.ts
@@ -1,10 +1,23 @@
-import translations from "./translations.json"
+import translations from "./translations.json";
 
 export type Languages = keyof typeof translations;
-export type Paths = keyof typeof translations[Languages]["translation"];
+export type Path = keyof typeof translations[Languages]["translation"];
 
-
-export default function(language: Languages, path: Paths): string {
-    return translations[language]["translation"][path];
+export default function translate(language: Languages, path: Path): string {
+  return translations[language].translation[path];
 }
 
+export function keyboard_translation(
+  language: Languages,
+  array: Array<Array<string>>
+) {
+  const out = array.reduce((array: Array<Array<string>>, row: string[]) => {
+    let kbd_row = row.reduce((btns, button) => {
+      btns.push(translate(language, button as Path));
+      return btns;
+    }, []);
+    array.push(kbd_row);
+    return array;
+  }, []);
+  return out;
+}
