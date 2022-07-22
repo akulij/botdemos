96e3e5d8bd98d33c3154589e4ed20ae31e4be56c
akulij <48361256+akulij@users.noreply.github.com>
2022-07-22 14:03:13 +0500

    add translation to greeting

--- src/modules/greeting/index.ts
+++ src/modules/greeting/index.ts
@@ -1,7 +1,9 @@
 import { Telegraf, Context } from "telegraf";
+import translation, { Languages } from "modules/translation"
 
 function greting(ctx: Context) {
-    ctx.reply("Hello, my dear friend!")
+    let language_code = ctx.message.from.language_code as Languages;
+    ctx.reply(translation(language_code, "greeting"))
 }
 
 export default function (bot: Telegraf) {
