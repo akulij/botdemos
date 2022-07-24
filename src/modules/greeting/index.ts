340c82661cd7149fcab40a6ea1d00d21760a2455
akulij <48361256+akulij@users.noreply.github.com>
2022-07-24 11:49:32 +0500

    fix greeting fuction name

--- src/modules/greeting/index.ts
+++ src/modules/greeting/index.ts
@@ -2,11 +2,11 @@ import { Telegraf, Context } from 'telegraf';
 import translation, { Languages } from 'modules/translation';
 import keyboards from "modules/keyboards"
 
-function greting(ctx: Context) {
+function greeting(ctx: Context) {
   const language_code = ctx.message.from.language_code as Languages;
   ctx.reply(translation(language_code, 'greeting'), keyboards(language_code, "menu"));
 }
 
 export default function (bot: Telegraf) {
-  bot.start(greting);
+  bot.start(greeting);
 }
