e8de4a95fecd4f257ec00dced3ecf650e5c3b543
akulij <48361256+akulij@users.noreply.github.com>
2022-07-26 00:21:55 +0500

    error if no keyboard exists

--- src/modules/keyboards/index.ts
+++ src/modules/keyboards/index.ts
@@ -1,12 +1,19 @@
-import { Markup } from 'telegraf';
-import keyboards from "./keyboards.json"
-import { Languages, keyboard_translation } from "modules/translation"
+import { Markup } from "telegraf";
+import keyboards from "./keyboards.json";
+import { Languages, keyboard_translation } from "modules/translation";
 
 export type KeyboardName = keyof typeof keyboards;
 
-export default function(language: Languages, keyboard_name: KeyboardName, resize_keyboard: boolean = true) {
-    let keyboard = Markup.keyboard(keyboard_translation(language, keyboards[keyboard_name]))
-    if (resize_keyboard)
-        keyboard.reply_markup.resize_keyboard = true;
-    return keyboard;
+export default function (
+  language: Languages,
+  keyboard_name: KeyboardName,
+  resize_keyboard: boolean = true
+) {
+  if (!keyboards[keyboard_name])
+    console.error(`No keyboard ${keyboards[keyboard_name]} exists!`);
+  let keyboard = Markup.keyboard(
+    keyboard_translation(language, keyboards[keyboard_name])
+  );
+  if (resize_keyboard) keyboard.reply_markup.resize_keyboard = true;
+  return keyboard;
 }
