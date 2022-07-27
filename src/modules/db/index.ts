74e15b4bb2328b2fb8af14fe8e1526d37bc10ea7
akulij <48361256+akulij@users.noreply.github.com>
2022-07-27 19:52:28 +0500

    fix import User type for database module

--- src/modules/db/index.ts
+++ src/modules/db/index.ts
@@ -1,5 +1,6 @@
 import { Context } from 'telegraf';
 import * as prisma from './prisma';
+import { User } from './types';
 
 export function getUserFromCtx(ctx: Context) {
   return { name: ctx.message.from.username } as User;
