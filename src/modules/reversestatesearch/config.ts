d767fde8646e2a6b4492d0979709f1e366b99d41
akulij <48361256+akulij@users.noreply.github.com>
2022-07-27 18:50:47 +0500

    add more keyboards

--- src/modules/reversestatesearch/config.ts
+++ src/modules/reversestatesearch/config.ts
@@ -5,6 +5,9 @@ import { KeyboardName } from 'modules/keyboards';
 
 export default {
     execute_on: {
-        "personal_account": (ctx: Context) => sendMsg(ctx, "personal_account_msg" as Path, "personal_account_kbd" as KeyboardName)
+        "personal_account": (ctx: Context) => sendMsg(ctx, "personal_account_msg" as Path, "personal_account_kbd" as KeyboardName),
+        "menu": (ctx: Context) => sendMsg(ctx, "greeting", "menu"),
+        "my_servers": (ctx: Context) => sendMsg(ctx, "my_servers_msg", "my_servers_kbd"),
+        "support": (ctx: Context) => sendMsg(ctx, "support_msg"),
     }
 }
