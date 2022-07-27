3c6b7621301a7f538563b2e30ea3f02b60b8bebf
akulij <48361256+akulij@users.noreply.github.com>
2022-07-27 23:40:32 +0500

    replace keyboard template to real list of user servers

--- src/modules/reversestatesearch/config.ts
+++ src/modules/reversestatesearch/config.ts
@@ -1,5 +1,5 @@
 import { Context } from 'telegraf';
-import sendMsg from 'modules/simplesend';
+import sendMsg, { messageIfKeyboard } from 'modules/simplesend';
 import { Path } from 'modules/translation';
 import { KeyboardName } from 'modules/keyboards';
 
@@ -7,7 +7,7 @@ export default {
   execute_on: {
     personal_account: (ctx: Context) => sendMsg(ctx, 'personal_account_msg' as Path, 'personal_account_kbd' as KeyboardName),
     menu: (ctx: Context) => sendMsg(ctx, 'greeting', 'menu'),
-    my_servers: (ctx: Context) => sendMsg(ctx, 'my_servers_msg', 'my_servers_kbd'),
+    my_servers: (ctx: Context) => messageIfKeyboard(ctx, 'my_servers_msg', 'no_servers_msg', 'my_servers_kbd'),
     support: (ctx: Context) => sendMsg(ctx, 'support_msg'),
     topup_balance: (ctx: Context) => sendMsg(ctx, 'topup_balance_msg'),
   },
