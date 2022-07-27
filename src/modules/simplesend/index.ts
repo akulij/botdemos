db0740f5e3d75d4e760afd692e075c7e179c778b
akulij <48361256+akulij@users.noreply.github.com>
2022-07-27 23:38:33 +0500

    add function with message deoends on keyboard existance

--- src/modules/simplesend/index.ts
+++ src/modules/simplesend/index.ts
@@ -2,7 +2,11 @@ import { Context } from 'telegraf';
 import translate, { Languages, Path } from 'modules/translation';
 import keyboards, { KeyboardName } from 'modules/keyboards';
 
-export default async (ctx: Context, message_name: Path, keyboard?: KeyboardName) => {
+export default async function sendMsg(
+  ctx: Context,
+  message_name: Path,
+  keyboard?: KeyboardName,
+) {
   const languageCode = ctx.message.from.language_code as Languages;
   if (keyboard) {
     ctx.reply(
@@ -12,4 +16,17 @@ export default async (ctx: Context, message_name: Path, keyboard?: KeyboardName)
   } else {
     ctx.reply(await translate(ctx, languageCode, message_name));
   }
-};
+}
+
+export function messageIfKeyboard(
+  ctx: Context,
+  msg_kbd: Path,
+  msg_nokbd: Path,
+  keyboard?: KeyboardName,
+) {
+  if (keyboard) {
+    sendMsg(ctx, msg_kbd, keyboard);
+  } else {
+    sendMsg(ctx, msg_nokbd);
+  }
+}
