bf4b02e6381e44550678637c91d87aa4754cff32
akulij <48361256+akulij@users.noreply.github.com>
2022-07-28 00:36:53 +0500

    replace require to es2016 style import

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import { Telegraf } from 'telegraf';
 import logger from 'modules/logger';
 import greeting from './modules/greeting';
 import reversestatesearch from './modules/reversestatesearch';
-
-require('dotenv').config();
+import 'dotenv/config';
 
 if (typeof process.env.BOTTOKEN !== 'string') {
   logger.error('Pass BOTTOKEN as enviroment variable or in .env file!');
