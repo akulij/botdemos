5b49eacb363ee5ebed9d6325b270d6033295e708
akulij <48361256+akulij@users.noreply.github.com>
2022-07-30 14:23:18 +0500

    add ydb driver initialization

--- src/modules/db/ydb.ts
+++ src/modules/db/ydb.ts
@@ -1,3 +1,11 @@
+import 'dotenv/config';
+import { Driver, getCredentialsFromEnv } from 'ydb-sdk';
+
+const endpoint = process.env.YANDEX_DATABASE_ENDPOINT;
+const database = process.env.YANDEX_DATABASE_EMPLACEMENT;
+const authService = getCredentialsFromEnv();
+const driver = new Driver({ endpoint, database, authService });
+
 export function getUserFromCtx() {
 
 }
