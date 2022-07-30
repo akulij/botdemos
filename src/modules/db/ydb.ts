3da32265077266d7701d9d6030c23fb3d4615525
akulij <48361256+akulij@users.noreply.github.com>
2022-07-30 14:27:11 +0500

    initialize ydb driver

--- src/modules/db/ydb.ts
+++ src/modules/db/ydb.ts
@@ -1,10 +1,18 @@
 import 'dotenv/config';
 import { Driver, getCredentialsFromEnv } from 'ydb-sdk';
+import logger from 'modules/logger';
 
 const endpoint = process.env.YANDEX_DATABASE_ENDPOINT;
 const database = process.env.YANDEX_DATABASE_EMPLACEMENT;
 const authService = getCredentialsFromEnv();
 const driver = new Driver({ endpoint, database, authService });
+
+driver.ready(1000).then(async (ready) => {
+  if (!ready) {
+    logger.error('Driver has not become ready in 10 seconds!');
+    process.exit(1);
+  }
+});
 
 export async function getUserFromCtx() {
 
